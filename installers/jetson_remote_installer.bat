1bbe47cffb4b11a9afff3674052c726cadab34c9
euanchua06 <euanchua06@gmail.com>
2025-12-01 14:20:17 +0800

    Update jetson_remote_installer.bat

--- installers/jetson_remote_installer.bat
+++ installers/jetson_remote_installer.bat
@@ -19,24 +19,25 @@ echo Running with administrator privileges...
 echo.
 timeout /t 2 /nobreak >nul
 
-:: Create Scripts directory
-echo [1/3] Creating C:\Scripts directory...
-if not exist "C:\Scripts" (
-    mkdir "C:\Scripts"
-    echo       Created C:\Scripts
-) else (
-    echo       C:\Scripts already exists
+:: Remove old Scripts directory if it exists
+echo [1/3] Preparing C:\Scripts directory...
+if exist "C:\Scripts" (
+    echo       Removing old version...
+    rmdir /s /q "C:\Scripts" 2>nul
+    echo       Old version removed
 )
+mkdir "C:\Scripts"
+echo       Created fresh C:\Scripts directory
 echo.
 
 :: Create connection batch file
-echo [2/3] Creating connect_jetson.bat...
+echo [2/3] Installing connect_jetson.bat...
 (
 echo @echo off
 echo echo Connecting to Jetson Nano at 192.168.0.191...
 echo mstsc /v:192.168.0.191
 ) > "C:\Scripts\connect_jetson.bat"
-echo       Created connect_jetson.bat
+echo       Installed connect_jetson.bat
 echo.
 
 :: Add to PATH
